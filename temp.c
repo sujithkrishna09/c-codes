93b9e41d3d4495907e343eb21e9ee6d856df9365
sujithkrishna09 <garlapatisujithkrishna@gmail.com>
2024-11-29 23:04:53 +0530

    Update temp.c

--- temp.c
+++ temp.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() 
 { 
-float f,c ;
-scanf("%f",&f);
+float fa,c ;
 printf("ENTER TEMPERATURE IN FEHRENHEIT DEGREES: ");
+ scanf("%f",&fa);
  c=5/9*(f-32);
  printf("temp in centigrade degrees=%f\n",c);
 }

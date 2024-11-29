a2ab85c0742e9139e24f7069be085f65033081cd
sujithkrishna09 <garlapatisujithkrishna@gmail.com>
2024-11-29 23:03:16 +0530

    Update compoundinterest.c

--- compoundinterest.c
+++ compoundinterest.c
@@ -1,17 +1,11 @@
+#include <stdio.h>
 #include <math.h>
 int main()
 {
-int p,r,n,t,a,b,c,d,e;
-printf("enter the value of p: \n");
-printf("enter the value of r: \n");
-printf("enter the value of n: \n");
-printf("enter the value of t: \n");
+int p,r,n,t;
+float c;
+printf("enter the value of p,r,n,t: \n");
 scanf("%d%d%d%d",&p,&r,&n,&t);
-scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-a=n+r/n;
-b=pow(a,n);
-c=pow(b,t);
-d=p*c;
-e=d-p;
+c=pow(1+(r/n),n*t);
 printf("the value of compound interest:%d",e);
 }

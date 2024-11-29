e4b502e10a3962cc1e8e08e1489e2d04d064fd22
sujithkrishna09 <garlapatisujithkrishna@gmail.com>
2024-11-29 22:45:15 +0530

    Update simpleinterest.c

--- simpleinterest.c
+++ simpleinterest.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 int main()
 {
-float p,r,t,S;
-printf("enter the value of p :");
-scanf("%d",&p);
-printf("enter the value of r :");
-scanf("%d",&r);
-printf("enter the value of t :");
-scanf("%d",&t);
-scanf("%d",&S);
+int p,;
+float r,t,s;
+printf("enter the value of p,r,t,s :");
+scanf("%d %f %f %f",&p,&r,&t,&s);
 S=p*r*t/100;
-printf("the value of simple interest :%d\n",S);
+printf("the value of simple interest :%f\n",S);
 }
